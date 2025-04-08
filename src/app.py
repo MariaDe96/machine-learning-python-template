8224d72d08ee10bbafce53987d29e6b085254ed7
MariaDe96 <mariadelgadof19.midf@gmail.com>
2025-04-08 10:24:26 +0000

    ml

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+from dotenv import load_dotenv
+load_dotenv()
 
 # your code here
+df = pd.read_csv("data/raw/data.csv")
+print(df.head())
